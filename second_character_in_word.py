8da5247d85154de7dcecbb1c13c7759afcbe0650
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-24 11:12:34 +0530

    Create second_character_in_word.py

---
+++ second_character_in_word.py
@@ -0,0 +1,11 @@
+def second_character(arg_1):
+    # Complete this function
+    return arg_1[1]
+word = input()
+# Call the second_character function
+print(second_character(word))
+
+"""
+input:MATHS
+output:A
+"""
