3b57e22e153b104f6ad9af727af307607c288ce4
balaji <76476394+Balaji041@users.noreply.github.com>
2023-09-24 11:09:09 +0530

    Create perimeter_of_square.py

---
+++ perimeter_of_square.py
@@ -0,0 +1,12 @@
+def perimeter_of_square(arg_1):
+    # Write your code here
+    return arg_1*4
+side = int(input())
+result = perimeter_of_square(side)
+print(result)
+
+
+"""
+input:3
+output:12
+"""
